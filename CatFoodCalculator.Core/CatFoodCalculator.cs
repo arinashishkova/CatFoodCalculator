436ba68d0a865725a6aa358ff6c955ed65950d9d
Arina Siskova <ariwksadenly@gmail.com>
2026-09-09 18:15:30 +0300

    Add project files.

---
+++ CatFoodCalculator.Core/CatFoodCalculator.cs
@@ -0,0 +1,55 @@
+﻿namespace CatFoodCalculator.Core
+{
+    public static class CatFoodCalculator
+    {
+        public static bool TryCalculate(
+            decimal weightKg,
+            decimal caloriesPerKg,
+            decimal caloriesPer100g,
+            int days,
+            out decimal dailyFoodGrams,
+            out decimal totalFoodKg,
+            out string error)
+        {
+            dailyFoodGrams = 0;
+            totalFoodKg = 0;
+            error = "";
+
+            // weight check
+            if (weightKg <= 0)
+            {
+                error = "Kassi kaal peab olema suurem kui 0.";
+                return false;
+            }
+
+            // calories per 1 kg check
+            if (caloriesPerKg <= 0)
+            {
+                error = "Kalorinorm peab olema suurem kui 0.";
+                return false;
+            }
+
+            // calorie per 100 g check
+            if (caloriesPer100g <= 0)
+            {
+                error = "Toidu kalorsus peab olema suurem kui 0.";
+                return false;
+            }
+            
+            // days count check
+            if(days <= 0)
+            {
+                error = "Päevade arv peab olema suurem kui 0.";
+                return false;
+            }
+
+            // calculations
+            decimal dailyCalories = weightKg * caloriesPerKg;
+            dailyFoodGrams = dailyCalories / caloriesPer100g * 100;
+            totalFoodKg = dailyFoodGrams * days / 1000;
+
+
+            return true;
+        }   
+    }
+}
